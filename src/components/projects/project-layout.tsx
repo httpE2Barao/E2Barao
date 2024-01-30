21e65414d148116753d520703e4ee5783018422e
httpE2Barao <elias.barao@hotmail.com>
2024-01-30 17:36:54 -0300

    projects layout

--- src/components/projects/project-layout.tsx
+++ src/components/projects/project-layout.tsx
@@ -1,16 +1,43 @@
 import { projectInterface } from "@/app/Projetos/page";
 import Image from "next/image";
+import { useTheme } from "../switchers/switchers";
 
 export const ProjectLayout = ({ list }: { list: projectInterface[] }) => {
+
+    const { theme } = useTheme();
+
     return (
-        <section className="grid grid-cols-4 gap-4 px-4">
+        <>
+            {/* <h1 className={`${theme === 'dark' ? 'text-white' : 'text-black'}
+            text-8xl mx-4 mt-8 mb-14 uppercase font-semibold`}
+            >Projects</h1> */}
 
-            { list.map((project, index) => (
-                <div key={index} className={`${index < 2 ? 'col-span-2' : 'col-span-1'}`}>
-                    <Image src={`/images/project_${project.name}.png`} alt={`${project.alt}`} width={650} height={550} />
-                </div>
-            ))}
+            <section className="grid grid-cols-2 gap-4 px-4">
+                {list.map((project, index) => (
+                    <>
+                        <figure key={index} className={`figure relative`}>
 
-        </section>
+                            <img src={`/images/project_${project.name}.png`} alt={`${project.alt}`}
+                                className="z-1"
+                            />
+
+                            <figcaption className="figcaption absolute inset-0 z-2
+                                flex flex-col items-center justify-end text-xl px-10 py-7 text-white
+                            ">
+                                <span className="uppercase z-10 flex flex-row items-center gap-20 font-bold">
+                                    <button className="h-[50px] px-10 rounded bg-white hover:bg-azul-claro text-black tracking-wider
+                                        ">ver site</button>
+                                    <h3 className="text-3xl my-10">{project.name}</h3>
+                                    <button className="h-[50px] px-10 rounded bg-white hover:bg-azul-claro text-black tracking-wider
+                                        ">repositório</button>
+                                </span>
+                                <p className="z-10 text-justify">{project.abt}</p>
+                            </figcaption>
+
+                        </figure>
+                    </>
+                ))}
+            </section>
+        </>
     );
 };
