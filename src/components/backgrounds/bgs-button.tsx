870a768a6ec1d0a118d86b583106bab470d10ff6
httpE2Barao <elias.barao@hotmail.com>
2024-02-10 20:17:23 -0300

    Update bgs-button.tsx

--- src/components/backgrounds/bgs-button.tsx
+++ src/components/backgrounds/bgs-button.tsx
@@ -4,18 +4,19 @@ interface ButtonProps {
   text: string;
   index: number;
   theme: string;
+  onClick: () => void;
 }
 
-export const Button = (props: ButtonProps) => {
+export const Button: React.FC<ButtonProps> = ({ index, text, onClick }) => {
 
   const { theme } = useTheme();
 
   return (
-    <button id={`scrollButton-${props.index}`} className={`btn-${props.index} ${theme==='dark' ? 'text-black bg-white' : 'text-white bg-black'} 
+    <button id={`scrollButton-${index}`} className={`btn-${index} ${theme==='dark' ? 'text-black bg-white' : 'text-white bg-black'} 
     icon-animation  rounded-full font-bold text-base 
     py-2 px-4 md:py-4 md:px-8 md:text-xl xl:py-7 xl:px-12 xl:text-4xl`}>
       <p className="content-animation">
-        {props.text}
+        {text}
       </p>
     </button>
   );
