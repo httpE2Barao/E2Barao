08153bb17b3e3d07d2b2bf549428f419bf99d491
httpE2Barao <elias.barao@hotmail.com>
2024-03-28 13:33:51 -0300

    atualização do texto de resumo das experiências

--- src/components/backgrounds/resume.tsx
+++ src/components/backgrounds/resume.tsx
@@ -14,8 +14,8 @@ export const ResumeAbt = (props: iResumeAbt) => {
     text-center font-semibold text-xl px-4 py-10 pt-60 mx-auto max-w-[2000px]`}>
       <p>
         {language==='pt-BR' 
-        ? 'Movido por uma paixão inabalável pela tecnologia, sou um desenvolvedor front-end altamente proficiente que domina as ferramentas e linguagens mais recentes para criar soluções inovadoras e de alta qualidade. Anseio por novos desafios e mantenho-me atualizado com as últimas tendências e tecnologias, sempre buscando aprimorar minhas habilidades e conhecimentos de forma proativa e dedicada.' 
-        : 'Driven by an unwavering passion for technology, I am a highly proficient front-end developer who masters the latest tools and languages to create innovative and high-quality solutions. I crave new challenges and keep myself updated with the latest trends and technologies, always striving to improve my skills and knowledge proactively and dedicatedly.'}
+        ? 'Como um desenvolvedor front-end freelancer, ofereço soluções inovadoras e de alta qualidade para os meus clientes. Minha paixão inabalável pela programação e design me impulsionam a criar experiências excepcionais para os usuários. Cada projeto é uma oportunidade para inovar. Busco constantemente maneiras criativas de resolver problemas e melhorar a experiência do usuário. Abaixo você pode selecionar e ver sobre as minhas experiências detalhadas sobre minha trajetória profissional e pessoal.' 
+        : 'As a freelance front-end developer, I offer innovative and high-quality solutions to my clients. My unwavering passion for programming and design drives me to create exceptional user experiences. Each project is an opportunity to innovate. I constantly seek creative ways to solve problems and improve the user experience. Below, you can select and view my detailed experiences about my professional and personal journey.'}
       </p>
     </section>
   )
