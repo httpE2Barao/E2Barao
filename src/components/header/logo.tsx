601184c8630dd1187157cfb2d18449e817cd43a7
httpE2Barao <elias.barao@hotmail.com>
2024-10-22 20:46:02 -0300

    Update logo.tsx

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -7,7 +7,7 @@ export const Logo = () => {
 
   return (
     <>
-      <Image src={`/images/icon-logo${theme==='dark'&&'-dark'}.png`} alt={"Logo 'EB'"}
+      <Image src={`/images/icon-logo${theme==='dark'?'-dark':''}.png`} alt={"Logo 'EB'"}
         className="hover:cursor-pointer w-30 h-30 md:w-30 md:h-30 lg:w-24 lg:h-24 4k:ml-10"
         width={100}
         height={100}
